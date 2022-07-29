18496fb6ecd70d4e89afeee5acc5fd593bfc48ee
AlexMyz <akomatoznik@gmail.com>
2022-07-29 15:21:52 +0300

    added adaptive

--- parts/page.php
+++ parts/page.php
@@ -1,28 +1,32 @@
 <main class="page">
 	<div class="container">
 		<section class="preview">
-			<div class="description">
-				<h1>Яркие гаджеты на любой вкус и день</h1>
-				<p>Мы собрали лучшие гаджеты от лучших брендов, чтобы порадовать вас свежими новинками digital-рынка. </p>
-				<a href="/all-products/all-products.php">Смотреть каталог</a>
+			<div class="preview__column">
+				<div class="description">
+					<h1>Яскраві гаджети <br> на будь-який смак та день</h1>
+					<p>Ми зібрали найкращі гаджети від найкращих брендів, щоб порадувати вас свіжими новинками digital-ринку.</p>
+					<a href="/all-products/all-products.php">Дивитися каталог</a>
+				</div>
 			</div>
-			<div class="product">
-				<img src="assets/img/preview-background.png">
-				<div class="bg-gradient"></div>
-				<img class="product-img" src="assets/img/drone.png">
-				<div class="product-description">
-					<h1>DJI Mavic Pro</h1>
-					<p>32 MP Sphere Panoramas
-						Foldable & Portable 3-Axis
-						Gimbal & 4K Camera</p>
-					<h3>3890 грн.</h3>
+			<div class="preview__column">
+				<div class="product">
+					<img class="bg-img" src="assets/img/preview-background.png">
+					<div class="bg-gradient"></div>
+					<img class="product-img" src="assets/img/drone.png">
+					<div class="product-description">
+						<h1>DJI Mavic Pro</h1>
+						<p>32 MP Sphere Panoramas
+							Foldable & Portable 3-Axis
+							Gimbal & 4K Camera</p>
+						<h3>3890 грн.</h3>
+					</div>
 				</div>
 			</div>
 		</section>
 
-		<div class="hot-slider">
+		<section class="hot-slider">
 			<div class="head">
-				<h1>Горячие поступления</h1>
+				<h2>Горячие поступления</h1>
 				<ul>
 					<li>Все</li>
 					<li>Телефоны</li>
@@ -31,206 +35,310 @@
 					<li>Игрушки</li>
 				</ul>
 			</div>
-			<div class="clearfix"></div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
-					<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
-					<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
-					<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
-					<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
+			<div class="mycustom-scroll" data-mcs-theme="3d-dark">
+				<div class="hot-slider__slider">
+					<div class="hot-slider__column">
+						<div class="product-card">
+							<h2>Iphone X</h2>
+							<img src="assets/img/product-img.png">
+							<table>
+								<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
+								<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
+								<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
+								<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
+								<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
+							</table>
+							<h3>22 000 грн.</h3>
+							<a href="">
+								<img src="assets/img/more.svg">
+							</a>
+						</div>
+					</div>
+					<div class="hot-slider__column">
+						<div class="product-card">
+							<h2>Iphone X</h2>
+							<img src="assets/img/product-img.png">
+							<table>
+								<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
+								<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
+								<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
+								<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
+								<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
+							</table>
+							<h3>22 000 грн.</h3>
+							<a href="">
+								<img src="assets/img/more.svg">
+							</a>
+						</div>
+					</div>
+					<div class="hot-slider__column">
+						<div class="product-card">
+							<h2>Iphone X</h2>
+							<img src="assets/img/product-img.png">
+							<table>
+								<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
+								<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
+								<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
+								<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
+								<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
+							</table>
+							<h3>22 000 грн.</h3>
+							<a href="">
+								<img src="assets/img/more.svg">
+							</a>
+						</div>
+					</div>
+					<div class="hot-slider__column">
+						<div class="product-card">
+							<h2>Iphone X</h2>
+							<img src="assets/img/product-img.png">
+							<table>
+								<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
+								<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
+								<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
+								<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
+								<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
+							</table>
+							<h3>22 000 грн.</h3>
+							<a href="">
+								<img src="assets/img/more.svg">
+							</a>
+						</div>
+					</div>
+					<div class="hot-slider__column">
+						<div class="product-card">
+							<h2>Iphone X</h2>
+							<img src="assets/img/product-img.png">
+							<table>
+								<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
+								<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
+								<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
+								<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
+								<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
+							</table>
+							<h3>22 000 грн.</h3>
+							<a href="">
+								<img src="assets/img/more.svg">
+							</a>
+						</div>
+					</div>
+					<div class="hot-slider__column">
+						<div class="product-card">
+							<h2>Iphone X</h2>
+							<img src="assets/img/product-img.png">
+							<table>
+								<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
+								<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
+								<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
+								<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
+								<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
+							</table>
+							<h3>22 000 грн.</h3>
+							<a href="">
+								<img src="assets/img/more.svg">
+							</a>
+						</div>
+					</div>
+					<div class="hot-slider__column">
+						<div class="product-card">
+							<h2>Iphone X</h2>
+							<img src="assets/img/product-img.png">
+							<table>
+								<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
+								<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
+								<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
+								<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
+								<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
+							</table>
+							<h3>22 000 грн.</h3>
+							<a href="">
+								<img src="assets/img/more.svg">
+							</a>
+						</div>
+					</div>
+					<div class="hot-slider__column">
+						<div class="product-card">
+							<h2>Iphone X</h2>
+							<img src="assets/img/product-img.png">
+							<table>
+								<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
+								<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
+								<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
+								<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
+								<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
+							</table>
+							<h3>22 000 грн.</h3>
+							<a href="">
+								<img src="assets/img/more.svg">
+							</a>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
-					<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
-					<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
-					<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
-					<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
+		</section>
+
+
+		<section class="offers">
+			<div class="offers__column">
+				<div class="card">
+					<img src="assets/img/offer-backgraund.png">
+					<div class="bg-gradient"></div>
+					<h2>Поступление новых самокатов</h1>
+					<h3>Электротранспорт снова в моде</h3>
+				</div>
 			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
-					<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
-					<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
-					<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
-					<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
+			<div class="offers__column">
+				<div class="card">
+					<img src="assets/img/offer-backgraund.png">
+					<div class="bg-gradient"></div>
+					<h2>Поступление новых самокатов</h1>
+					<h3>Электротранспорт снова в моде</h3>
+				</div>
 			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td><strong>Корпус:</strong></td><td>Металл, Стекло</td></tr>
-					<tr><td><strong>Влагозащита:</strong></td><td>IP67</td></tr>
-					<tr><td><strong>Цвет:</strong></td><td>Черный</td></tr>
-					<tr><td><strong>Тип экрана:</strong></td><td>IPS</td></tr>
-					<tr><td><strong>Размер экрана:</strong></td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
+		</section>
+
+		<section class="catalog">
+			<h1 class="h2">Каталог товаров</h1>
+			<div class="catalog__columns">
+				<div class="catalog__column">
+					<div class="product-card">
+						<h1>Iphone X</h1>
+						<img src="assets/img/product-img.png">
+						<table>
+							<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
+							<tr><td>Влагозащита:</td><td>IP67</td></tr>
+							<tr><td>Цвет:</td><td>Черный</td></tr>
+							<tr><td>Тип экрана:</td><td>IPS</td></tr>
+							<tr><td>Размер экрана:</td><td>4,7</td></tr>
+						</table>
+						<h3>22 000 грн.</h3>
+						<a>
+							<img src="assets/img/more.svg">
+						</a>
+					</div>
+				</div>
+				<div class="catalog__column">
+					<div class="product-card">
+						<h1>Iphone X</h1>
+						<img src="assets/img/product-img.png">
+						<table>
+							<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
+							<tr><td>Влагозащита:</td><td>IP67</td></tr>
+							<tr><td>Цвет:</td><td>Черный</td></tr>
+							<tr><td>Тип экрана:</td><td>IPS</td></tr>
+							<tr><td>Размер экрана:</td><td>4,7</td></tr>
+						</table>
+						<h3>22 000 грн.</h3>
+						<a>
+							<img src="assets/img/more.svg">
+						</a>
+					</div>
+				</div>
+				<div class="catalog__column">
+					<div class="product-card">
+						<h1>Iphone X</h1>
+						<img src="assets/img/product-img.png">
+						<table>
+							<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
+							<tr><td>Влагозащита:</td><td>IP67</td></tr>
+							<tr><td>Цвет:</td><td>Черный</td></tr>
+							<tr><td>Тип экрана:</td><td>IPS</td></tr>
+							<tr><td>Размер экрана:</td><td>4,7</td></tr>
+						</table>
+						<h3>22 000 грн.</h3>
+						<a>
+							<img src="assets/img/more.svg">
+						</a>
+					</div>
+				</div>
+				<div class="catalog__column">
+					<div class="product-card">
+						<h1>Iphone X</h1>
+						<img src="assets/img/product-img.png">
+						<table>
+							<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
+							<tr><td>Влагозащита:</td><td>IP67</td></tr>
+							<tr><td>Цвет:</td><td>Черный</td></tr>
+							<tr><td>Тип экрана:</td><td>IPS</td></tr>
+							<tr><td>Размер экрана:</td><td>4,7</td></tr>
+						</table>
+						<h3>22 000 грн.</h3>
+						<a>
+							<img src="assets/img/more.svg">
+						</a>
+					</div>
+				</div>
+				<div class="catalog__column">
+					<div class="product-card">
+						<h1>Iphone X</h1>
+						<img src="assets/img/product-img.png">
+						<table>
+							<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
+							<tr><td>Влагозащита:</td><td>IP67</td></tr>
+							<tr><td>Цвет:</td><td>Черный</td></tr>
+							<tr><td>Тип экрана:</td><td>IPS</td></tr>
+							<tr><td>Размер экрана:</td><td>4,7</td></tr>
+						</table>
+						<h3>22 000 грн.</h3>
+						<a>
+							<img src="assets/img/more.svg">
+						</a>
+					</div>
+				</div>
+				<div class="catalog__column">
+					<div class="product-card">
+						<h1>Iphone X</h1>
+						<img src="assets/img/product-img.png">
+						<table>
+							<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
+							<tr><td>Влагозащита:</td><td>IP67</td></tr>
+							<tr><td>Цвет:</td><td>Черный</td></tr>
+							<tr><td>Тип экрана:</td><td>IPS</td></tr>
+							<tr><td>Размер экрана:</td><td>4,7</td></tr>
+						</table>
+						<h3>22 000 грн.</h3>
+						<a>
+							<img src="assets/img/more.svg">
+						</a>
+					</div>
+				</div>
+				<div class="catalog__column">
+					<div class="product-card">
+						<h1>Iphone X</h1>
+						<img src="assets/img/product-img.png">
+						<table>
+							<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
+							<tr><td>Влагозащита:</td><td>IP67</td></tr>
+							<tr><td>Цвет:</td><td>Черный</td></tr>
+							<tr><td>Тип экрана:</td><td>IPS</td></tr>
+							<tr><td>Размер экрана:</td><td>4,7</td></tr>
+						</table>
+						<h3>22 000 грн.</h3>
+						<a>
+							<img src="assets/img/more.svg">
+						</a>
+					</div>
+				</div>
+				<div class="catalog__column">
+					<div class="product-card">
+						<h1>Iphone X</h1>
+						<img src="assets/img/product-img.png">
+						<table>
+							<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
+							<tr><td>Влагозащита:</td><td>IP67</td></tr>
+							<tr><td>Цвет:</td><td>Черный</td></tr>
+							<tr><td>Тип экрана:</td><td>IPS</td></tr>
+							<tr><td>Размер экрана:</td><td>4,7</td></tr>
+						</table>
+						<h3>22 000 грн.</h3>
+						<a>
+							<img src="assets/img/more.svg">
+						</a>
+					</div>
+				</div>
 			</div>
+			
+
 		</div>
-		<div class="clearfix"></div>
-		<div class="offers">
-			<div class="card">
-				<img src="assets/img/offer-backgraund.png">
-				<div class="bg-gradient"></div>
-				<h1>Поступление новых самокатов</h1>
-				<h3>Электротранспорт снова в моде</h3>
-			</div>
-			<div class="card">
-				<img src="assets/img/offer-backgraund.png">
-				<div class="bg-gradient"></div>
-				<h1>Поступление новых самокатов</h1>
-				<h3>Электротранспорт снова в моде</h3>
-			</div>
-		</div>
-		<div class="catalog">
-			<h1 class="h1">Каталог товаров</h1>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
-					<tr><td>Влагозащита:</td><td>IP67</td></tr>
-					<tr><td>Цвет:</td><td>Черный</td></tr>
-					<tr><td>Тип экрана:</td><td>IPS</td></tr>
-					<tr><td>Размер экрана:</td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
-			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
-					<tr><td>Влагозащита:</td><td>IP67</td></tr>
-					<tr><td>Цвет:</td><td>Черный</td></tr>
-					<tr><td>Тип экрана:</td><td>IPS</td></tr>
-					<tr><td>Размер экрана:</td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
-			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
-					<tr><td>Влагозащита:</td><td>IP67</td></tr>
-					<tr><td>Цвет:</td><td>Черный</td></tr>
-					<tr><td>Тип экрана:</td><td>IPS</td></tr>
-					<tr><td>Размер экрана:</td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
-			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
-					<tr><td>Влагозащита:</td><td>IP67</td></tr>
-					<tr><td>Цвет:</td><td>Черный</td></tr>
-					<tr><td>Тип экрана:</td><td>IPS</td></tr>
-					<tr><td>Размер экрана:</td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
-			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
-					<tr><td>Влагозащита:</td><td>IP67</td></tr>
-					<tr><td>Цвет:</td><td>Черный</td></tr>
-					<tr><td>Тип экрана:</td><td>IPS</td></tr>
-					<tr><td>Размер экрана:</td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
-			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
-					<tr><td>Влагозащита:</td><td>IP67</td></tr>
-					<tr><td>Цвет:</td><td>Черный</td></tr>
-					<tr><td>Тип экрана:</td><td>IPS</td></tr>
-					<tr><td>Размер экрана:</td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
-			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
-					<tr><td>Влагозащита:</td><td>IP67</td></tr>
-					<tr><td>Цвет:</td><td>Черный</td></tr>
-					<tr><td>Тип экрана:</td><td>IPS</td></tr>
-					<tr><td>Размер экрана:</td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
-			</div>
-			<div class="product-card">
-				<h1>Iphone X</h1>
-				<img src="assets/img/product-img.png">
-				<table>
-					<tr><td>Корпус:</td><td>Металл, Стекло</td></tr>
-					<tr><td>Влагозащита:</td><td>IP67</td></tr>
-					<tr><td>Цвет:</td><td>Черный</td></tr>
-					<tr><td>Тип экрана:</td><td>IPS</td></tr>
-					<tr><td>Размер экрана:</td><td>4,7</td></tr>
-				</table>
-				<h3>22 000 грн.</h3>
-				<button>
-					<img src="assets/img/more.svg">
-				</button>
-			</div>
-		</div>
-		<div class="clearfix"></div>
 	</div>
 </main>
